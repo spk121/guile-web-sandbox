c56c8ccd6b9a6059094c6303f5d358a0308dfa28
Mike Gran <spk121@yahoo.com>
2023-07-15 19:08:20 -0700

    Update scm-style-repl.scm

--- module/sandy/scm-style-repl.scm
+++ module/sandy/scm-style-repl.scm
@@ -1,5 +1,6 @@
 ;;;; Copyright (C) 1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2014
 ;;;; Free Software Foundation, Inc.
+;;;; Copyright (C) 2023 Michael L. Gran
 ;;;;
 ;;;; This library is free software; you can redistribute it and/or
 ;;;; modify it under the terms of the GNU Lesser General Public
